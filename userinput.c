715a835ebd6576c0e19432b9356a2e9b8c5e360a
Maqa001 <mma.malikov@mail.ru>
2024-02-11 19:44:09 +0400

    Input File

---
+++ userinput.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(){
+    int number;
+
+    printf("Enter a number: ");
+
+    //saves number 
+    scanf("%d", &number);
+
+    printf("Your number is %d", number);
+    return 0;
+
+}
